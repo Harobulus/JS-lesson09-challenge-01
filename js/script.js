97e9bd19dacbe4a6bf9078b58f40a8271e8d936b
Haruna Bulus <harobulus@gmail.com>
2023-07-20 19:42:39 +0100

    Coded assignment of potluck dishes

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+// 
+const assignButton = document.querySelector(".assign");
+// 
+const assignedItems = document.querySelector(".assigned-items");
 
 addGuestButton.addEventListener("click", function () {
     const guest = guestInput.value;
@@ -43,3 +47,33 @@ const updateGuestCount = function () {
     };
 
 };
+
+// Select Assigned Items & Build an Array.
+const assignItems = function () {
+
+    const potluckItems = ["potato salad", "hummus", "cookies", "fruit",
+     "Halloumi", "Veggie", "Mozzarella", "cheese tomato tart",
+      "Zucchini noodles", "Avocado", "stuffed peppadews", "bean isreali"];
+
+ // Select Elements & Loop Through the Array.  
+      
+let allGuests = document.querySelectorAll(".guest-list li");
+
+    for (let guest of allGuests) {
+        let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+        assignedItems.append(listItem);
+        potluckItems.splice(randomPotluckIndex, 1);
+    };
+    
+};
+
+// Add an Event Listener & Update PotluckItems Array
+
+assignButton.addEventListener("click", function () {
+    assignItems();
+    assignButton.disabled = true;
+  });
+
